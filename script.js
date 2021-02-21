e8ebd42d8f0227867b74f2b7e47c19ac298b0326
holczur <holcz.norbert@gmail.com>
2021-02-21 16:59:17 +0100

    navigation is ready

---
+++ script.js
@@ -0,0 +1,20 @@
+const closeMenu = document.querySelector('.close')
+const openMenu = document.querySelector('.open')
+const hiddenNav = document.querySelector('.hidden-nav')
+const mobileNav = document.querySelector('.mobile-nav-head')
+const welcomeText = document.querySelector('.welcome .container')
+
+openMenu.addEventListener('click', () => {
+    hiddenNav.classList.remove('invisible');
+    mobileNav.classList.add('invisible');
+    welcomeText.classList.add('hide');
+})
+
+closeMenu.addEventListener('click', () => {
+    hiddenNav.classList.add('invisible');
+    mobileNav.classList.remove('invisible');
+    welcomeText.classList.remove('hide');
+
+})
+
+
